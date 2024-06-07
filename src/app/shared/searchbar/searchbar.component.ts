02e4bf881d862a839c97c91c9a3aeaf5234bf6ea
waglarry <emmanuelacquaah2468@gmail.com>
2024-06-07 04:32:43 +0000

    feat: implement function to emit searhTerm value

--- src/app/shared/searchbar/searchbar.component.ts
+++ src/app/shared/searchbar/searchbar.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-searchbar',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './searchbar.component.html',
   styleUrl: './searchbar.component.scss'
 })
 export class SearchbarComponent {
+	@Output() searchTermValue: EventEmitter<string> = new EventEmitter<string>();
+	public searchTerm: string = '';
 
+	public handleSearchFilter(event: string) {
+		this.searchTermValue.emit(event);
+	}
 }
