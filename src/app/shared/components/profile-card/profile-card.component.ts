811a09d81e2addf078d90a9ec7c736ff8d16092a
waglarry <emmanuelacquaah2468@gmail.com>
2024-06-07 11:17:21 +0000

    feat: list menu on card

--- src/app/shared/components/profile-card/profile-card.component.ts
+++ src/app/shared/components/profile-card/profile-card.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { INavlinks } from '../navbar/navbar.interface';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-profile-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './profile-card.component.html',
   styleUrl: './profile-card.component.scss'
 })
 export class ProfileCardComponent {
-
+  public cardItems: INavlinks[] = [
+    {
+      label: 'Your Profile',
+      link: '/profile'
+    },
+    {
+      label: 'Settings',
+      link: '/settings'
+    }
+  ];
 }
