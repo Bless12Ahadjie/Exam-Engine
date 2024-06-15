1e1fd2db06a7986ff03952f3123d0d8c4ffbea3b
Ahadjie-Bless <bless.ahadjie@amalitech.com>
2024-06-15 09:29:11 +0000

    feat:  implemented upcoming exam table

--- src/app/shared/pages/student-layout/student-layout.component.ts
+++ src/app/shared/pages/student-layout/student-layout.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import {StudentDashboardComponent} from "../../components/student-dashboard/student-dashboard.component";
 
 @Component({
   selector: 'app-student-layout',
   standalone: true,
   imports: [
     NavbarComponent,
+    StudentDashboardComponent,
   ],
   templateUrl: './student-layout.component.html',
   styleUrl: './student-layout.component.scss'
