e4342b84691a65392f29cb9d4ce756c41f7d0870
Ahadjie-Bless <bless.ahadjie@amalitech.com>
2024-06-16 09:01:16 +0000

    feat: initialized start modal

---
+++ src/app/shared/components/start-mosal/start-mosal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start-mosal',
+  standalone: true,
+  imports: [],
+  templateUrl: './start-mosal.component.html',
+  styleUrl: './start-mosal.component.scss'
+})
+export class StartMosalComponent {
+
+}
--- src/app/shared/components/upcoming-exam-table/upcoming-exam-table.component.ts
+++ src/app/shared/components/upcoming-exam-table/upcoming-exam-table.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
+import { Component,signal } from '@angular/core';
 import {TitleCasePipe, UpperCasePipe} from "@angular/common";
+import {StartMosalComponent} from "../start-mosal/start-mosal.component";
 
 @Component({
   selector: 'app-upcoming-exam-table',
   standalone: true,
   imports: [
     UpperCasePipe,
-    TitleCasePipe
+    TitleCasePipe,
+    StartMosalComponent
   ],
   templateUrl: './upcoming-exam-table.component.html',
   styleUrl: './upcoming-exam-table.component.scss'
 })
 export class UpcomingExamTableComponent {
+
+  isModalOpen = signal(false);
 
   tableHead = [
     {
@@ -39,5 +43,13 @@ export class UpcomingExamTableComponent {
       update: 'update'
     }
   ]
+
+
+
+  showModal(){
+    this.isModalOpen.update(()=> true)
+    console.log('clicked')
+  }
+
 
 }
