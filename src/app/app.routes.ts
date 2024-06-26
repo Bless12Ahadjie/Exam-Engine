44d8c2d4bf7740172c8bc5a0be7c443660e4fe82
Ahadjie-Bless <bless.ahadjie@amalitech.com>
2024-06-26 10:51:42 +0000

    feat: implemented confirmation page one

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { LoginPageComponent } from './shared/pages/auth/login-page/login-page.component';
 import { SignupPageComponent } from './shared/pages/auth/signup-page/signup-page.component';
 import { StudentLayoutComponent } from './shared/pages/student-layout/student-layout.component';
+import { ConfirmationPage1Component } from './shared/components/confirmation-page-1/confirmation-page-1.component';
+import { StudentDashboardComponent } from './shared/components/student-dashboard/student-dashboard.component';
 
 export const routes: Routes = [
   {
@@ -21,12 +23,18 @@ export const routes: Routes = [
   {
     path: 'student',
     component: StudentLayoutComponent,
-    // children: [
-    //   {
-    //     path: 'dashboard',
+    children: [
+      {
+        path: 'dashboard',
+        component: StudentDashboardComponent
 
-    //   }
-    // ]
+      },
+      {
+        path: 'confirm-1',
+        component: ConfirmationPage1Component
+
+      }
+    ]
 
 
   }
