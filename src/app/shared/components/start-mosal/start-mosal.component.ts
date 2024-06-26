9a134cc7ef2bb003c4a2f0c7939d77c9433bb069
Ahadjie-Bless <bless.ahadjie@amalitech.com>
2024-06-26 09:40:34 +0000

    feat: fixed modal not closing

--- src/app/shared/components/start-mosal/start-mosal.component.ts
+++ src/app/shared/components/start-mosal/start-mosal.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, HostListener,Output} from '@angular/core';
+import {Component, EventEmitter,Output} from '@angular/core';
 
 @Component({
   selector: 'app-start-mosal',
@@ -8,17 +8,11 @@ import {Component, EventEmitter, HostListener,Output} from '@angular/core';
   styleUrl: './start-mosal.component.scss'
 })
 export class StartMosalComponent {
-  @Output() closeModalEvent = new EventEmitter<boolean>(false)
+  @Output() closeModalEvent = new EventEmitter<boolean>();
 
-  @HostListener('document:click', ['$event'])
-  public onClick(event: MouseEvent): void {
-    const clickedElement = event.target as HTMLElement;
-    const isClickedInsideModal = clickedElement.closest('.modal-content');
-    const isClickedInsideButton = clickedElement.closest('.cursor-pointer');
 
-    if (!isClickedInsideModal || !isClickedInsideButton) {
-      this.closeModalEvent.emit(false);
-    }
+  closeModal(){
+    this.closeModalEvent.emit(false);
   }
 
 }
