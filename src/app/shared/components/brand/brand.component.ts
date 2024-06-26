623515b59dbc46584162efea8d04ff8724b7f28e
waglarry <emmanuelacquaah2468@gmail.com>
2024-06-26 22:06:30 +0000

    refactor: update brand to take size as input

--- src/app/shared/components/brand/brand.component.ts
+++ src/app/shared/components/brand/brand.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-brand',
   standalone: true,
   imports: [],
   templateUrl: './brand.component.html',
-  styleUrl: './brand.component.scss'
+  styleUrl: './brand.component.scss',
 })
 export class BrandComponent {
+  size = input<string>('');
 
+  textClasses(): string {
+    return `bg-gradient-to-r from-[#D428E1F5] to-[#39558E] bg-clip-text text-transparent text-[${this.size()}] font-bold`;
+  }
 }
