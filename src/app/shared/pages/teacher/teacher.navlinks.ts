f6008dd83d353b9df051fb7e967641d27b354bb2
waglarry <emmanuelacquaah2468@gmail.com>
2024-06-27 19:49:00 +0000

    refactor: update teacher navlinks

--- src/app/shared/pages/teacher/teacher.navlinks.ts
+++ src/app/shared/pages/teacher/teacher.navlinks.ts
@@ -3,21 +3,21 @@ import { INavLinks } from './components/sidebar/sidebar.interface';
 export const teacherNavLinks: INavLinks = {
   links: [
     {
-      title: 'Dashboard',
-      defaultIconSrc: '/assets/icons/dashboard.svg',
-      routerLink: 'dashboard',
+      title: 'Home',
+      defaultIconSrc: '/assets/icons/home.svg',
+      routerLink: 'home',
     },
     {
-      title: 'Create Question',
-      defaultIconSrc: '/assets/icons/create-question.svg',
-      routerLink: 'projects',
-      children: [{ title: '', routerLink: '' }],
+      title: 'Manage Exams',
+      defaultIconSrc: '/assets/icons/manage-exams.svg',
+      routerLink: 'manage-exams',
+      // children: [{ title: '', routerLink: '' }],
     },
     {
-      title: ' Study Materials',
-      defaultIconSrc: '/assets/icons/study.svg',
-      routerLink: 'manage-users',
-      children: [{ title: '', routerLink: '' }],
+      title: 'Analytics',
+      defaultIconSrc: '/assets/icons/analytics.svg',
+      routerLink: 'analytics',
+      // children: [{ title: '', routerLink: '' }],
     },
     {
       title: 'Settings',
