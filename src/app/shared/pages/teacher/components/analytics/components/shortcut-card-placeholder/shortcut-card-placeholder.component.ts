6632a7d629d97cca41d4ac052153f6bb98e14e0e
waglarry <emmanuelacquaah2468@gmail.com>
2024-06-27 21:57:16 +0000

    feat: create shortcut-card-placeholder component

---
+++ src/app/shared/pages/teacher/components/analytics/components/shortcut-card-placeholder/shortcut-card-placeholder.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shortcut-card-placeholder',
+  standalone: true,
+  imports: [],
+  templateUrl: './shortcut-card-placeholder.component.html',
+  styleUrl: './shortcut-card-placeholder.component.scss'
+})
+export class ShortcutCardPlaceholderComponent {
+
+}
