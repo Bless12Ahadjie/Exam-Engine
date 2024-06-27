12fcc061aa578f7e9c83686090e72e5bfc788f8e
waglarry <emmanuelacquaah2468@gmail.com>
2024-06-27 21:07:32 +0000

    feat: add analytics to teacher routes

--- src/app/shared/pages/teacher/teacher.routes.ts
+++ src/app/shared/pages/teacher/teacher.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./components/home/home.component";
+import { AnalyticsComponent } from "./components/analytics/analytics.component";
 
 export const routes: Routes = [
     {
         path: 'home',
         component: HomeComponent
-    }
+    },
+    {
+        path: 'analytics',
+        component: AnalyticsComponent
+    },
 ];
