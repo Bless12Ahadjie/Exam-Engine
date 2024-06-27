b688159b9757550496f0919bd331e0fc6063224f
waglarry <emmanuelacquaah2468@gmail.com>
2024-06-27 21:10:35 +0000

    feat: create shortcut card component

---
+++ src/app/shared/pages/teacher/components/analytics/components/shortcut-card/shortcut-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shortcut-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './shortcut-card.component.html',
+  styleUrl: './shortcut-card.component.scss'
+})
+export class ShortcutCardComponent {
+
+}
