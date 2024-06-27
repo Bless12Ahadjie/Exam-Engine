314d5dcb2de1f130ed8bb8f2a36f6ca6e93f7f4c
waglarry <emmanuelacquaah2468@gmail.com>
2024-06-27 21:55:33 +0000

    feat: design shortcut card component

--- src/app/shared/pages/teacher/components/analytics/components/shortcut-card/shortcut-card.component.ts
+++ src/app/shared/pages/teacher/components/analytics/components/shortcut-card/shortcut-card.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IShortcutCard } from '../../interfaces/shortcut-card.interface';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-shortcut-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './shortcut-card.component.html',
-  styleUrl: './shortcut-card.component.scss'
+  styleUrl: './shortcut-card.component.scss',
 })
 export class ShortcutCardComponent {
-
+  @Input({ required: true }) props: IShortcutCard = {
+  	iconSrc: '',
+  	label: '',
+  	value: '',
+  	link: '',
+  };
 }
