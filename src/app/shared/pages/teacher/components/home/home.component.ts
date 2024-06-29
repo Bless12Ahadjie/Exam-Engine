c29b1d887f403f4903e2b04b9159552a07f4cecc
waglarry <emmanuelacquaah2468@gmail.com>
2024-06-29 20:50:40 +0000

    feat: integrate question and form component into home component

--- src/app/shared/pages/teacher/components/home/home.component.ts
+++ src/app/shared/pages/teacher/components/home/home.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, signal } from '@angular/core';
+import { FormComponent } from '../form/form.component';
+import { Question } from '../../interfaces/question.interface';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  @ViewChild(FormComponent) formComponent!: FormComponent;
+  toggledCreateQuestion = signal<boolean>(true);
+  questions = signal<Question[]>([]);
 
+  toggleCreateQuestion() {
+    this.toggledCreateQuestion.update((prev) => !prev);
+  }
+
+  handleQuestionsData(questions: Question[]) {
+    this.questions.set(questions);
+    console.log('Received questions data:', this.questions());
+  }
+
+  sendQuestions() {
+    if (this.formComponent) {
+      this.questions.set(this.formComponent.questions);
+      console.log('Sending questions data:', this.questions());
+    }
+  }
 }
