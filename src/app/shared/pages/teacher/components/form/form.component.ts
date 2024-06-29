6ddbdc33f6edb41ca6b9649dba7b6ecb9053ee4a
waglarry <emmanuelacquaah2468@gmail.com>
2024-06-29 20:49:21 +0000

    feat: create question and form component

--- src/app/shared/pages/teacher/components/form/form.component.ts
+++ src/app/shared/pages/teacher/components/form/form.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, output } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { QuestionComponent } from '../question/question.component';
+import { Question } from '../../interfaces/question.interface';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [QuestionComponent, ReactiveFormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss',
 })
-export class FormComponent {}
+export class FormComponent implements OnInit {
+  questionsEmitter = output<Question[]>();
+  questions: Question[] = [];
+
+  ngOnInit() {
+    this.addQuestion();
+  }
+
+  addQuestion() {
+    const newQuestion: Question = {
+      type: 'multiple-choice',
+      text: 'Untitled Question',
+      label: `Question ${this.questions.length + 1}`,
+      options: [],
+      correctAnswers: [],
+    };
+    this.questions.push(newQuestion);
+  }
+
+  editQuestion(question: Question) {
+    // Implement logic to edit the question
+    console.log('Editing:', question);
+  }
+
+  deleteQuestion(question: Question) {
+    this.questions = this.questions.filter((q) => q !== question);
+  }
+
+  duplicateQuestion(question: Question) {
+    const duplicatedQuestion = {
+      ...question,
+      label: question.label + ' (Copy)',
+    };
+    this.questions.push(duplicatedQuestion);
+  }
+
+  sendQuestions() {
+    this.questionsEmitter.emit(this.questions);
+  }
+}
--- src/app/shared/pages/teacher/components/question/question.component.ts
+++ src/app/shared/pages/teacher/components/question/question.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AbstractControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Question } from '../../interfaces/question.interface';
 
 @Component({
   selector: 'app-question',
   standalone: true,
-  imports: [],
+  imports: [ ReactiveFormsModule, FormsModule],
   templateUrl: './question.component.html',
   styleUrl: './question.component.scss'
 })
 export class QuestionComponent {
+  @Input() question!: Question;
+  @Input() control!: AbstractControl<any, any> | null;
 
+  @Output() edit = new EventEmitter<void>();
+  @Output() delete = new EventEmitter<void>();
+  @Output() duplicate = new EventEmitter<void>();
+
+  addOption() {
+    if (!this.question.options) {
+      this.question.options = [];
+    }
+    this.question.options.push({ label: `Option ${this.question.options.length + 1}`, value: '' });
+  }
+
+  deleteOption(index: number) {
+    if (this.question.options) {
+      this.question.options.splice(index, 1);
+    }
+  }
 }
