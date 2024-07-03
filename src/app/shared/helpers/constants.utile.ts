ad9434c4aafa6a04e265b4cb4bf3e226b0dde5ce
waglarry <emmanuelacquaah2468@gmail.com>
2024-07-03 07:19:23 +0000

    feat: create constants and string helpers

---
+++ src/app/shared/helpers/constants.utile.ts
@@ -0,0 +1,30 @@
+import { PayLoadData } from '../../interfaces/token.interface';
+
+export const ACCESS_TOKEN_KEY: string = 'EXAM_ENGINE_TOKEN';
+
+export const persistedSave = (key: string, value: string) => {
+  sessionStorage.setItem(key, value);
+};
+
+export const persistedGet = (key: string) => {
+  return sessionStorage.getItem(key);
+};
+
+export const persistedRemove = (key: string) => {
+  sessionStorage.removeItem(key);
+};
+
+export function decodeJwt(token: String): PayLoadData | null {
+  if (!token) {
+    return null;
+  }
+
+  const parts = token.split('.');
+  if (parts.length !== 3) {
+    return null;
+  }
+
+  const payload = JSON.parse(atob(parts[1]));
+
+  return payload;
+}
