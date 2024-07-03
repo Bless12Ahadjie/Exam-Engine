2fc0693a110b37d2ba4215b0dd3ce6ee5b22973f
waglarry <emmanuelacquaah2468@gmail.com>
2024-07-03 07:22:41 +0000

    feat: create auth and error interceptors

---
+++ src/app/interceptors/auth/auth.interceptor.ts
@@ -0,0 +1,40 @@
+import {
+  HttpEvent,
+  HttpInterceptorFn,
+  HttpResponse,
+} from '@angular/common/http';
+import { inject } from '@angular/core';
+import { TokenService } from '../../services/token/token.service';
+import { AuthService } from '../../services/auth/auth.service';
+import { decodeJwt } from '../../shared/helpers/constants.utile';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
+  const tokenService = inject(TokenService);
+  const accessToken: string | null = tokenService.getToken();
+
+  if (accessToken && !req.url.includes('/auth')) {
+    console.log('AccessToken: ', accessToken);
+
+    const payloadData = decodeJwt(accessToken);
+    console.log('payloadData: ', payloadData);
+
+    if (payloadData) {
+      const isExpired = payloadData.exp
+        ? payloadData.exp < Date.now() / 1000
+        : false;
+
+      if (isExpired) {
+        // authService.logoutUser();
+      }
+    }
+
+    const authReq = req.clone({
+      headers: req.headers.set('Authorization', 'Bearer ' + accessToken),
+    });
+
+    return next(authReq);
+  }
+
+  return next(req);
+};
