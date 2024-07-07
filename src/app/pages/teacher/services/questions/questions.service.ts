f8a2d02eafbecc11e024e2a38289d2b1f41cd5f7
waglarry <emmanuelacquaah2468@gmail.com>
2024-07-07 06:51:10 +0000

    feat: implement a method to create questions

--- src/app/pages/teacher/services/questions/questions.service.ts
+++ src/app/pages/teacher/services/questions/questions.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../../environments/environment.development';
+import { UserStore } from '../../../../store/user/user.store';
+import { IResponse } from '../../../../interfaces/response.interface';
+import { ExamQuestion } from '../../interfaces/question.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class QuestionsService {
+  private readonly userStore = inject(UserStore);
 
-  constructor() { }
+  private _httpClient = inject(HttpClient);
+
+  constructor() {}
+
+  public createQuestion(questions: ExamQuestion) {
+    return this._httpClient.post<IResponse>(
+      `${
+        environment.BACKEND_API_BASE_URL
+      }/exam-engine/api/v1/teacher/create/new-questions/${this.userStore.userId()}`,
+      questions
+    );
+  }
 }
