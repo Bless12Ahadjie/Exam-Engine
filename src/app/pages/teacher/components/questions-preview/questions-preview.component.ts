298fbd0a47ab2295cc4c0b8f6f9039c93424ae85
waglarry <emmanuelacquaah2468@gmail.com>
2024-07-07 16:02:13 +0000

    feat: implement logic to get and load question from storage onto the preview screen

--- src/app/pages/teacher/components/questions-preview/questions-preview.component.ts
+++ src/app/pages/teacher/components/questions-preview/questions-preview.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { Question } from '../../interfaces/question.interface';
+import { persistedGet } from '../../../../shared/helpers/constants.utile';
 
 @Component({
   selector: 'app-questions-preview',
   standalone: true,
   imports: [],
   templateUrl: './questions-preview.component.html',
-  styleUrl: './questions-preview.component.scss'
+  styleUrl: './questions-preview.component.scss',
 })
-export class QuestionsPreviewComponent {
+export class QuestionsPreviewComponent implements OnInit {
+  questions = signal<Question[]>([]);
 
+  ngOnInit(): void {
+    const questionsData = persistedGet('exam_engine_questions');
+    if (questionsData) {
+      const questions = JSON.parse(questionsData);
+      this.questions.set(questions);
+    }
+  }
 }
