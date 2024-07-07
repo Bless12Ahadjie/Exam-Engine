7ba4fe36349a40ca60f46b4731d11f8ec23c6cd1
waglarry <emmanuelacquaah2468@gmail.com>
2024-07-07 13:13:50 +0000

    feat: implement functionality to save and retreive settings data from storage

--- src/app/pages/teacher/components/questions-settings/questions-settings.component.ts
+++ src/app/pages/teacher/components/questions-settings/questions-settings.component.ts
@@ -1,12 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, inject, output } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ExamSettings } from '../../interfaces/question.interface';
+import { ToasterService } from '../../../../shared/components/toaster/services/toaster.service';
 
 @Component({
   selector: 'app-questions-settings',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './questions-settings.component.html',
-  styleUrl: './questions-settings.component.scss'
+  styleUrl: './questions-settings.component.scss',
 })
 export class QuestionsSettingsComponent {
+  settingsChanged = output<ExamSettings>();
+  settingsForm!: FormGroup;
 
+  private fb = inject(FormBuilder);
+  private _toaster = inject(ToasterService);
+
+  ngOnInit(): void {
+    this.initSettingsForm();
+    this.loadSettingsFromStorage();
+  }
+
+  initSettingsForm() {
+    this.settingsForm = this.fb.group({
+      questionTitle: ['', Validators.required],
+      questionInstruction: ['', Validators.required],
+      questionStartTime: ['', Validators.required],
+      questionEndTime: ['', Validators.required],
+      questionReceivers: [
+        '',
+        [Validators.required, Validators.pattern('[^,]+(,[^,]+)*')],
+      ],
+    });
+  }
+
+  saveSettings() {
+    if (this.settingsForm.valid) {
+      const settingsData: ExamSettings = this.settingsForm.value;
+      this.saveToStorage(settingsData);
+      this.settingsChanged.emit(settingsData);
+      this.showToast('Settings saved successfully.', 'success');
+    } else {
+      this.showToast('Please fill in all settings fields.', 'error');
+    }
+  }
+
+  saveToStorage(settingsData: ExamSettings) {
+    sessionStorage.setItem(
+      'exam_engine_settings',
+      JSON.stringify(settingsData)
+    );
+  }
+
+  loadSettingsFromStorage() {
+    const savedSettings = sessionStorage.getItem('exam_engine_settings');
+    if (savedSettings) {
+      const settingsData: ExamSettings = JSON.parse(savedSettings);
+      this.settingsForm.patchValue(settingsData);
+    }
+  }
+
+  private showToast(message: string, type: 'error' | 'success') {
+    this._toaster.showToast({
+      message,
+      type,
+      duration: 3000,
+    });
+  }
 }
