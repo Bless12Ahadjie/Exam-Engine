57b667ba94179505f5b52f996716cbb1d77598b4
waglarry <emmanuelacquaah2468@gmail.com>
2024-07-20 23:08:30 +0000

    feat: create manage exams page component

---
+++ src/app/pages/teacher/manage-exams/manage-exams.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-exams',
+  standalone: true,
+  imports: [],
+  templateUrl: './manage-exams.component.html',
+  styleUrl: './manage-exams.component.scss'
+})
+export class ManageExamsComponent {
+
+}
