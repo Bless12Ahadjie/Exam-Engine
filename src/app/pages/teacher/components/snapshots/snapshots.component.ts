afdcb4abe9eeefb2aaf87ff26f63df43a9d3b947
waglarry <emmanuelacquaah2468@gmail.com>
2024-07-31 10:35:16 +0000

    feat: create snapshots component

---
+++ src/app/pages/teacher/components/snapshots/snapshots.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-snapshots',
+  standalone: true,
+  imports: [],
+  templateUrl: './snapshots.component.html',
+  styleUrl: './snapshots.component.scss'
+})
+export class SnapshotsComponent {
+
+}
