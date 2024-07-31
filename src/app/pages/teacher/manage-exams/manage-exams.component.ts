c147cde18173c4f3472f4492c6fe430e6c6df715
waglarry <emmanuelacquaah2468@gmail.com>
2024-07-31 11:34:37 +0000

    refactor: make manage exams component the parent route

--- src/app/pages/teacher/manage-exams/manage-exams.component.ts
+++ src/app/pages/teacher/manage-exams/manage-exams.component.ts
@@ -1,65 +1,11 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { TableComponent } from '../../../shared/components/table/table.component';
-import { ManageExamsService } from '../services/manage-exams/manage-exams.service';
-import { IResponseData, TableData } from './manage-exams.interface';
-import { ToasterService } from '../../../shared/components/toaster/services/toaster.service';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-manage-exams',
   standalone: true,
-  imports: [TableComponent],
+  imports: [RouterOutlet],
   templateUrl: './manage-exams.component.html',
   styleUrl: './manage-exams.component.scss',
 })
-export class ManageExamsComponent implements OnInit {
-  columns: string[] = [
-    'Title',
-    'Status',
-    'Start Time',
-    'End Time',
-    'Pass Mark',
-  ];
-  questions = signal<TableData[]>([]);
-  isLoading: boolean = false;
-
-  _manageExamsService = inject(ManageExamsService);
-  _toaster = inject(ToasterService);
-
-  ngOnInit(): void {
-    this.fetchData();
-  }
-
-  fetchData() {
-    this.isLoading = true;
-
-    this._manageExamsService.getManageExamsQuestions().subscribe({
-      next: (data) => {
-        this.isLoading = false;
-        this.responseHandler(data);
-      },
-      error: (error) => {
-        this.isLoading = false;
-
-        const errorMessage = `Something went wrong in getting the table data. ${error.message}`;
-        this.showToast(errorMessage, 'error');
-        console.error(error);
-      },
-    });
-  }
-
-  responseHandler(response: IResponseData) {
-    if (response.status === 200) {
-      this.questions.set(response.questions);
-    } else {
-      this.showToast(response.message, 'error');
-    }
-  }
-
-  private showToast(message: string, type: 'error' | 'success') {
-    this._toaster.showToast({
-      message,
-      type,
-      duration: 3000,
-    });
-  }
-}
+export class ManageExamsComponent {}
